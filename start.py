9e500e34a4a0718a0089d12201f19b6890601a57
Ayushiyadav01 <91016502+Ayushiyadav01@users.noreply.github.com>
2022-06-15 15:36:59 +0530

    final base

--- start.py
+++ start.py
@@ -1,14 +1,7 @@
 import serial
-from socket import timeout
-# from serial import serial 
 
 ser = serial.Serial('COM5', 4800, timeout=2)
-ser.open()
-ser.read()
-print(ser.name)
+
 while True :
-    data = []
-    data.append(ser.readlines())
-    print(data)
-print(data)
-ser.close()
+    data_raw = ser.readline()
+    print(data_raw)
