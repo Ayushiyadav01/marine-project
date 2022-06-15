fd6b92bfc725b8af728c7307ed020674e020f9e5
Ayushiyadav01 <91016502+Ayushiyadav01@users.noreply.github.com>
2022-06-15 23:55:06 +0530

    all data added

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+from time import process_time_ns
 import serial
 
 ser = serial.Serial('COM5', 4800, timeout=2)
@@ -9,4 +10,11 @@ while True :
     data_li = list(data_raw.split(","))
     # print(data_li)
     if (data_li[0]=='b\'$GPGGA'):
-        print(data_li)
+        # print(data_li)
+
+        UTC = data_li[1]
+        print("UTC = ", UTC)
+        lat = data_li[2]
+        print("Latitude = ", lat, data_li[3])
+        long = data_li[4]
+        print("Longitude = ", long, data_li[5])
