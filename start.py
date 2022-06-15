5911e1aaafcd77c5beaf5f7743f27b404ddc9e0d
Ayushiyadav01 <91016502+Ayushiyadav01@users.noreply.github.com>
2022-06-15 17:01:36 +0530

    converted into list

--- start.py
+++ start.py
@@ -3,5 +3,10 @@ import serial
 ser = serial.Serial('COM5', 4800, timeout=2)
 
 while True :
-    data_raw = ser.readline()
-    print(data_raw)
+    data_raw = str(ser.readline())
+    # print(type(data_raw))
+    # print(data_raw)
+    data_li = list(data_raw.split(","))
+    # print(data_li)
+    if (data_li[0]=='b\'$GPGGA'):
+        print(data_li)
